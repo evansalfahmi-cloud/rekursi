3c9c9ec2179281147b8270d515a56ebedc118ea9
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2023-04-08 15:14:05 +0700

    rekursi hitung pangkat

--- Program.cs
+++ Program.cs
@@ -3,22 +3,25 @@
 using System ; 
 
 class reKursi {
-    public int hitungFaktorial (int n){
-        if (n == 0){
+    public int hitungPangkat (int n , int e){
+        if (e==0){
             return 1;
         }else {
-            return n * hitungFaktorial(n-1);
+            return n * hitungPangkat (n, e-1);
         }
     }
 }
 
 class demoRekursi {
     static void Main (){
-        reKursi obj = new reKursi();
+        reKursi obj = new reKursi ();
 
-        //memanggil metode hitung faktorial;
-        for (int i = 0 ; i <= 10 ; i ++){
-            Console.WriteLine(i + "! = "+ obj.hitungFaktorial(i));
+        int bilangan = 2;
+
+        //memanggil metode rerkursi
+        for (int i = 0 ; i < 10 ; i ++){
+            Console.WriteLine (bilangan + " ^ "+ i + " = " + obj.hitungPangkat(bilangan, i)); //2 pangkat i
         }
+        
     }
 }
