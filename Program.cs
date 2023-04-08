24e88615bce026712c9b4bf25876fa40566d5edc
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2023-04-08 15:07:41 +0700

    rekursi hitung faktorial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//contoh program rekursif
+
+using System ; 
+
+class reKursi {
+    public int hitungFaktorial (int n){
+        if (n == 0){
+            return 1;
+        }else {
+            return n * hitungFaktorial(n-1);
+        }
+    }
+}
+
+class demoRekursi {
+    static void Main (){
+        reKursi obj = new reKursi();
+
+        //memanggil metode hitung faktorial;
+        for (int i = 0 ; i <= 10 ; i ++){
+            Console.WriteLine(i + "! = "+ obj.hitungFaktorial(i));
+        }
+    }
+}
